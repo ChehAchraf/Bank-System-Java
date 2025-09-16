c2f647a5a15dcc32dfdd39bfcee7c571dc56c7e1
ChehAchraf <achrafchehboun@outlook.com>
2025-09-16 11:36:31 +0100

    add some feutures and classes

--- src/metier/Compte.java
+++ src/metier/Compte.java
@@ -1,7 +1,39 @@
 package metier;
 
+
+
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class Compte {
     protected String code;
     protected double solde;
+    protected List<Operation> listOperations;
     
+    public Compte(String code, double solde) {
+    		this.code = code;
+    		this.solde = solde;
+    		this.listOperations = new ArrayList<>();
+    }
+    
+    public String getCode() {
+        return code;
+    }
+
+    public double getSolde() {
+        return solde;
+    }
+    
+    public void verser(double montant, String source) {
+    		this.solde += montant;
+    }
+    
+    public List<Operation> getListOperations() {
+        return listOperations;
+    }
+
+    public abstract void showDetails();
+    public abstract void retirer(double montant, String destination);
+    public abstract double calculerInteret();
+
 }
---
+++ src/metier/Operation.java
@@ -0,0 +1,30 @@
+package metier;
+
+import java.util.UUID;
+import java.time.LocalDateTime;
+
+
+public abstract class Operation {
+    protected String code;
+    protected LocalDateTime date;
+    protected double montant;
+
+    	public Operation(double montant) {
+    		this.code = UUID.randomUUID().toString();
+    		this.date = LocalDateTime.now();
+    		this.montant = montant;
+    		
+    	}
+    
+    	public double getMontant() { 
+		return montant;
+	}
+    public LocalDateTime getDate() { 
+		return date;
+	}
+    public String getNumero() { 
+		return code; 
+	}
+    
+
+}
